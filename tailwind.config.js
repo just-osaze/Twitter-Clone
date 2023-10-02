50b50d916fa1179094153686c89f909460082a26
just-osaze <osaigbevo.osaz@gmail.com>
2023-10-02 15:59:46 +0100

    Initializing the Repo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      XBlue: '#1D9BF0',
+      white: '#FFFFFF',
+      gray: '#687684'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
